29dad5629c30a6f70e32a962464188eda9d07e6b
shibuya-n <a.duong2006@gmail.com>
2022-10-04 13:38:23 -0700

    new update 10/4

--- BlackJack/src/Dealer.java
+++ BlackJack/src/Dealer.java
@@ -12,12 +12,14 @@ public class Dealer {
     public static ArrayList<String> diamonds = new ArrayList<>();
 
 
-    public static String[] cardList = {"ace", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "jack", "queen", "king"};
+    public static String[] cardList = {"ace", "2", "3", "4", "5", "6", "7", "8", "9", "ten", "jack", "queen", "king"};
     public static double randomSuit = ((Math.random() * 4) + 1);
     public static int setSuit = (int)Math.round(randomSuit);
     public static String useSuit = "";
 
     public static int userHand = 0;
+
+    public static ArrayList<String> userHandCards = new ArrayList<>();
 
 
     public static void initialize(){
@@ -25,11 +27,11 @@ public class Dealer {
         suit();
     }
     public static void cardAdder() {
-        for (int i = 0; i < cardList.length; i++) {
-            hearts.add(cardList[i]);
-            spades.add(cardList[i]);
-            clubs.add(cardList[i]);
-            diamonds.add(cardList[i]);
+        for (String s : cardList) {
+            hearts.add(s);
+            spades.add(s);
+            clubs.add(s);
+            diamonds.add(s);
 
         }
     }
@@ -38,204 +40,118 @@ public class Dealer {
 
         if (setSuit == 1) {
             useSuit = useSuit + "hearts";
-            card();
+            getCard();
         }
         if (setSuit == 2) {
             useSuit = useSuit + "spades";
-            card();
+            getCard();
         }
         if (setSuit == 3){
             useSuit = useSuit + "clubs";
-            card();
+            getCard();
         }
         if (setSuit == 4){
             useSuit = useSuit + "diamonds";
-            card();
+            getCard();
         }
 
     }
 
-    public static void card() {
+    public static void getCard() {
         double randomCard = ((Math.random() * 13) + 1);
         int setCard = (int)Math.round(randomCard);
         int eRange = setCard - 1;
 
         //takes out the cards
         if (useSuit.equals("hearts")) {
-            for (int i = 2; i > 0; i--){
-                if (hearts.get(eRange).equals("ace")) {
-                    userHand += 11;
-                    hearts.remove(eRange);
-                }
-                if (hearts.get(eRange).equals("two")) {
-                    userHand += 2;
-                    hearts.remove(eRange);
-                }
-                if (hearts.get(eRange).equals("three")) {
-                    userHand += 3;
-                }
-                if (hearts.get(eRange).equals("four")) {
-                    userHand += 4;
-                }
-                if (hearts.get(eRange).equals("five")) {
-                    userHand += 5;
-                }
-                if (hearts.get(eRange).equals("six")) {
-                    userHand += 6;
-                }
-                if (hearts.get(eRange).equals("seven")) {
-                    userHand += 7;
-                }
-                if (hearts.get(eRange).equals("eight")) {
-                    userHand += 8;
-                }
-                if (hearts.get(eRange).equals("nine")) {
-                    userHand += 9;
-                }
-                if (hearts.get(eRange).equals("ten") || hearts.get(eRange).equals("jack") || hearts.get(eRange).equals("queen") || hearts.get(eRange).equals("king")) {
-                    userHand += 10;
-                }
-            }
+            if (hearts.get(eRange).equals("ace")) {
+                userHand += 11;
+                String card = hearts.remove(eRange);
+                userHandCards.add(card);
 
+
+            }
+            else if (hearts.get(eRange).equals("ten") || hearts.get(eRange).equals("jack") || hearts.get(eRange).equals("queen") || hearts.get(eRange).equals("king")) {
+                userHand += 10;
+                String card = hearts.remove(eRange);
+                userHandCards.add(card);
+            }
+            else {
+                int cardNumber = Integer.parseInt(hearts.get(eRange));
+                userHand += cardNumber;
+                String card = hearts.remove(eRange);
+                userHandCards.add(card);
+            }
 
         }
-        if (useSuit.equals("spades")) {
-            for (int i = 2; i > 0; i--) {
-                if (spades.get(eRange).equals("ace")) {
-                    userHand += 11;
-                    spades.remove(eRange);
-                }
-                if (spades.get(eRange).equals("two")) {
-                    userHand += 2;
-                    spades.remove(eRange);
-                }
-                if (spades.get(eRange).equals("three")) {
-                    userHand += 3;
-                    spades.remove(eRange);
-                }
-                if (spades.get(eRange).equals("four")) {
-                    userHand += 4;
-                    spades.remove(eRange);
-                }
-                if (spades.get(eRange).equals("five")) {
-                    userHand += 5;
-                    spades.remove(eRange);
-                }
-                if (spades.get(eRange).equals("six")) {
-                    userHand += 6;
-                    spades.remove(eRange);
-                }
-                if (spades.get(eRange).equals("seven")) {
-                    userHand += 7;
-                    spades.remove(eRange);
-                }
-                if (spades.get(eRange).equals("eight")) {
-                    userHand += 8;
-                    spades.remove(eRange);
-                }
-                if (spades.get(eRange).equals("nine")) {
-                    userHand += 9;
-                    spades.remove(eRange);
-                }
-                if (spades.get(eRange).equals("ten") || spades.get(eRange).equals("jack") || spades.get(eRange).equals("queen") || spades.get(eRange).equals("king")) {
-                    userHand += 10;
-                    spades.remove(eRange);
-                }
-            }
 
+        if (useSuit.equals("spades")) {
+            if (spades.get(eRange).equals("ace")) {
+                userHand += 11;
+                String card = spades.remove(eRange);
+                userHandCards.add(card);
+
+
+            }
+            else if (spades.get(eRange).equals("ten") || spades.get(eRange).equals("jack") || spades.get(eRange).equals("queen") || spades.get(eRange).equals("king")) {
+                userHand += 10;
+                String card = spades.remove(eRange);
+                userHandCards.add(card);
+            }
+            else {
+                int cardNumber = Integer.parseInt(spades.get(eRange));
+                userHand += cardNumber;
+                String card = spades.remove(eRange);
+                userHandCards.add(card);
+
+            }
         }
 
         if (useSuit.equals("clubs")) {
-            for (int i = 2; i > 0; i--) {
-                if (clubs.get(eRange).equals("ace")) {
-                    userHand += 11;
-                    clubs.remove(eRange);
-                }
-                if (clubs.get(eRange).equals("two")) {
-                    userHand += 2;
-                    clubs.remove(eRange);
-                }
-                if (clubs.get(eRange).equals("three")) {
-                    userHand += 3;
-                    clubs.remove(eRange);
-                }
-                if (clubs.get(eRange).equals("four")) {
-                    userHand += 4;
-                    clubs.remove(eRange);
-                }
-                if (clubs.get(eRange).equals("five")) {
-                    userHand += 5;
-                    clubs.remove(eRange);
-                }
-                if (clubs.get(eRange).equals("six")) {
-                    userHand += 6;
-                    clubs.remove(eRange);
-                }
-                if (clubs.get(eRange).equals("seven")) {
-                    userHand += 7;
-                    clubs.remove(eRange);
-                }
-                if (clubs.get(eRange).equals("eight")) {
-                    userHand += 8;
-                    clubs.remove(eRange);
-                }
-                if (clubs.get(eRange).equals("nine")) {
-                    userHand += 9;
-                    clubs.remove(eRange);
-                }
-                if (clubs.get(eRange).equals("ten") || clubs.get(eRange).equals("jack") || clubs.get(eRange).equals("queen") || clubs.get(eRange).equals("king")) {
-                    userHand += 10;
-                    clubs.remove(eRange);
-                }
+            if (clubs.get(eRange).equals("ace")) {
+                userHand += 11;
+                String card = clubs.remove(eRange);
+                userHandCards.add(card);
+
+            }
+            else if (clubs.get(eRange).equals("ten") || clubs.get(eRange).equals("jack") || clubs.get(eRange).equals("queen") || clubs.get(eRange).equals("king")) {
+                userHand += 10;
+                String card = clubs.remove(eRange);
+                userHandCards.add(card);
+            }
+            else {
+                int cardNumber = Integer.parseInt(clubs.get(eRange));
+                userHand += cardNumber;
+                String card = clubs.remove(eRange);
+                userHandCards.add(card);
+
             }
 
         }
         if (useSuit.equals("diamonds")) {
-            for (int i = 2; i > 0; i--) {
-                if (diamonds.get(eRange).equals("ace")) {
-                    userHand += 11;
-                    diamonds.remove(eRange);
-                }
-                if (diamonds.get(eRange).equals("two")) {
-                    userHand += 2;
-                    diamonds.remove(eRange);
-                }
-                if (diamonds.get(eRange).equals("three")) {
-                    userHand += 3;
-                    diamonds.remove(eRange);
-                }
-                if (diamonds.get(eRange).equals("four")) {
-                    userHand += 4;
-                    diamonds.remove(eRange);
-                }
-                if (diamonds.get(eRange).equals("five")) {
-                    userHand += 5;
-                    diamonds.remove(eRange);
-                }
-                if (diamonds.get(setCard - 1).equals("six")) {
-                    userHand += 6;
-                    diamonds.remove(eRange);
-                }
-                if (diamonds.get(eRange).equals("seven")) {
-                    userHand += 7;
-                    diamonds.remove(eRange);
-                }
-                if (diamonds.get(eRange).equals("eight")) {
-                    userHand += 8;
-                    diamonds.remove(eRange);
-                }
-                if (diamonds.get(eRange).equals("nine")) {
-                    userHand += 9;
-                    diamonds.remove(eRange);
-                }
-                if (diamonds.get(eRange).equals("ten") || diamonds.get(eRange).equals("jack") || diamonds.get(eRange).equals("queen") || diamonds.get(eRange).equals("king")) {
-                    userHand += 10;
-                    diamonds.remove(eRange);
-                }
+            if (diamonds.get(eRange).equals("ace")) {
+                userHand += 11;
+                String card = diamonds.remove(eRange);
+                userHandCards.add(card);
+
+            }
+            else if (diamonds.get(eRange).equals("ten") || diamonds.get(eRange).equals("jack") || diamonds.get(eRange).equals("queen") || diamonds.get(eRange).equals("king")) {
+                userHand += 10;
+                String card = diamonds.remove(eRange);
+                userHandCards.add(card);
+            }
+            else {
+                int cardNumber = Integer.parseInt(diamonds.get(eRange));
+                userHand += cardNumber;
+                String card = diamonds.remove(eRange);
+                userHandCards.add(card);
+
             }
         }
 
-        System.out.println(userHand);
+        System.out.println("Your cards: " + userHandCards);
+        System.out.println("Your total: " + userHand);
+
     }
 
 
