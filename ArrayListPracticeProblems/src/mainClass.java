b19b31ebf4214943f33d1238f8d65bd2ededca69
Andy Duong <a.duong2006@gmail.com>
2022-10-21 20:37:19 -0700

    recent commit

--- ArrayListPracticeProblems/src/mainClass.java
+++ ArrayListPracticeProblems/src/mainClass.java
@@ -5,6 +5,7 @@
 //        Array of size 4 in asc order: {5, 9, 1, 3, 4} entered, {1, 3, 4, 5, 9} returned
 //        Array of size 5 in desc order: {-25, -7, 1, 13, 40} entered, {40, 13, 1, -7, -25}
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class mainClass {
@@ -24,11 +25,17 @@ public class mainClass {
         if (userUpOrDown.equals("asc")) {
             order.sort();
 
+            System.out.println("Your ordered array: " );
+            System.out.println(Arrays.toString(userArray));
+
 
         }
-//        else if (userUpOrDown.equals("desc")) {
-//
-//        }
+        else if (userUpOrDown.equals("desc")) {
+            order.reverseSort();
+
+            System.out.println("Your ordered array (in descending order): ");
+            System.out.println(Arrays.toString(userArray));
+        }
 
         else {
             System.out.println("INVALID SYNTAX. PLEASE TRY AGAIN.");
@@ -46,8 +53,9 @@ public class mainClass {
         userArray = new int[arrayLength];
 
         for (int i = 0; i < arrayLength; i++) {
+            int count = i + 1;
             Scanner askUserValue = new Scanner(System.in);
-            System.out.println("What is your first value?  [INSERT NUMBER.]");
+            System.out.println("What is your " + count  + "th value?  [INSERT NUMBER.]");
             int userValue = Integer.parseInt(String.valueOf(askUserValue.nextLine()));
             userArray[i]  = userValue;
 
--- ArrayListPracticeProblems/src/order.java
+++ ArrayListPracticeProblems/src/order.java
@@ -1,37 +1,37 @@
-import java.util.Arrays;
+
 
 public class order {
 
-    public static int minIndex = 0;
-
-    public static int[] orderedArray = new int[mainClass.arrayLength];
-
     public static void sort(){
-        System.out.println(Arrays.toString(mainClass.userArray));
-        System.out.println(Arrays.toString(orderedArray));
+        int temp = 0;
+
 
         for (int i = 0; i < mainClass.arrayLength; i++)  {
 
-            minIndex = i;
-
             for (int x = i + 1; x < mainClass.arrayLength; x++) {
 
-                if (mainClass.userArray[x] < mainClass.userArray[minIndex]) {
-                    minIndex = x;
+                if (mainClass.userArray[i] > mainClass.userArray[x]) {
+                    temp = mainClass.userArray[i];
+                    mainClass.userArray[i] = mainClass.userArray[x];
+                    mainClass.userArray[x] = temp;
                 }
 
                 }
 
-            int temp = minIndex;
-            mainClass.userArray[i] = mainClass.userArray[minIndex];
-
-
-            System.out.println(minIndex);
-            System.out.println(mainClass.userArray[minIndex]);
 
             }
+
         }
-//        System.out.println("Your ordered array: " );
-//        System.out.println(Arrays.toString(orderedArray));
+    public static void reverseSort() {
+        sort();
+        int temp = 0;
+
+        for (int i = 0; i < mainClass.arrayLength / 2 ; i++) {
+            temp = mainClass.userArray[i];
+            mainClass.userArray[i] = mainClass.userArray[mainClass.arrayLength - (i + 1)];
+            mainClass.userArray[mainClass.arrayLength - (i + 1)] = temp;
+        }
+
+    }
     }
 
